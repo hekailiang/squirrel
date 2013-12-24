9597b02a55d54e72afc49a749d0eb2f66804d8c6
hekailiang <hekailiang@gmail.com>
2013-12-24 18:31:09 +0800

    Add transition end annotation

---
+++ squirrel-foundation/src/main/java/org/squirrelframework/foundation/fsm/annotation/TransitionEnd.java
@@ -0,0 +1,18 @@
+package org.squirrelframework.foundation.fsm.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Transition end listener annotation
+ * @author Henry.He
+ *
+ */
+@Target({METHOD})
+@Retention(RUNTIME)
+public @interface TransitionEnd {
+    String condition() default "";
+}
