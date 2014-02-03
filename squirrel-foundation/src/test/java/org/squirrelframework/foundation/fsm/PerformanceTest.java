d12e95754b97e6976976d665eea4808af89d97fb
hekailiang <hekailiang@gmail.com>
2014-02-03 11:39:22 +0800

    Add performance monitor overload test

--- squirrel-foundation/src/test/java/org/squirrelframework/foundation/fsm/PerformanceTest.java
+++ squirrel-foundation/src/test/java/org/squirrelframework/foundation/fsm/PerformanceTest.java
@@ -5,12 +5,15 @@ package org.squirrelframework.foundation.fsm;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import org.junit.Test;
 import org.squirrelframework.foundation.fsm.annotation.StateMachineParameters;
 import org.squirrelframework.foundation.fsm.impl.AbstractUntypedStateMachine;
+
+import com.google.common.base.Stopwatch;
 
 public class PerformanceTest {
 
@@ -27,20 +30,80 @@ public class PerformanceTest {
 
     @Test(timeout = 10000)
     public void manyTransitions() {
-        performTest(10000, false);
+        performTest(10000, false, null);
     }
     
     @Test
     public void manyTransitionWithMonitor() {
-        performTest(1000, true);
+        performTest(1000, true, null);
     }
     
-    void performTest(final int iterTimes, final boolean addPerfMonitor) {
+    @Test
+    public void comparePerfMonitorOverload() {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                calculatePi(100);
+            }
+        };
+        
+        int testIter = 100;
+        for(int i=0; i<4; ++i) {
+            performTest(testIter, false, task);
+            performTest(testIter, true, task);
+        }
+        
+        int iterTimes = 20;
+        float overloadTimes=0;
+        for(int i=0; i<iterTimes; ++i) {
+            Stopwatch watch1 = new Stopwatch().start();
+            performTest(testIter, false, task);
+            long time1 = watch1.stop().elapsedMillis();
+            System.out.println("Task 1 finished in "+time1+"ms.");
+            
+            Stopwatch watch2 = new Stopwatch().start();
+            performTest(testIter, true, task);
+            long time2 = watch2.stop().elapsedMillis();
+            System.out.println("Task 2 finished in "+time2+"ms.");
+            float overloadTime = time2-time1;
+            overloadTimes+=overloadTime;
+            System.out.println("--------------------------------------");
+        }
+        System.out.println("Average overload for each transition is "+
+                String.format("%.4f", overloadTimes/iterTimes/testIter/4)+"ms.");
+    }
+    
+    private BigDecimal calculatePi(int iterTimes) {
+        BigDecimal sum = new BigDecimal(0);        // final sum
+        BigDecimal term = new BigDecimal(0);       // term without sign
+        BigDecimal sign = new BigDecimal(1.0);     // sign on each term
+
+        BigDecimal one = new BigDecimal(1.0);
+        BigDecimal two = new BigDecimal(2.0);
+
+        for (int k = 0; k < iterTimes; k++) {
+           BigDecimal count = new BigDecimal(k); 
+           //term = 1.0/(2.0*k + 1.0);
+           BigDecimal temp1 = two.multiply(count);
+           BigDecimal temp2 = temp1.add(one);
+           term = one.divide(temp2,50,BigDecimal.ROUND_FLOOR);
+           //sum = sum + sign*term;
+           BigDecimal temp3 = sign.multiply(term);
+           sum = sum.add(temp3);
+           sign = sign.negate();
+        }
+        BigDecimal pi = new BigDecimal(0);
+        BigDecimal four = new BigDecimal(4);
+        pi = sum.multiply(four);
+        return pi;
+    }
+    
+    void performTest(final int iterTimes, final boolean addPerfMonitor, final Runnable task) {
         UntypedStateMachineBuilder builder = StateMachineBuilderFactory.create(StateMachineSample.class);
         Action<UntypedStateMachine, Object, Object, Object> action = new AnonymousAction<UntypedStateMachine, Object, Object, Object>() {
             @Override
             public void execute(Object from, Object to, Object event, Object context, UntypedStateMachine stateMachine) {
-                // System.out.println(to);
+                if(task!=null) task.run();
             }
         };
 
@@ -68,7 +131,7 @@ public class PerformanceTest {
                     long totalTimes = 2*4*iterTimes;
                     assertEquals(perfModel.getTotalTransitionInvokedTimes(), totalTimes);
                     assertEquals(perfModel.getTotalActionInvokedTimes(), totalTimes);
-                    System.out.println(perfModel);
+//                    System.out.println(perfModel);
                 }
             };
         }
