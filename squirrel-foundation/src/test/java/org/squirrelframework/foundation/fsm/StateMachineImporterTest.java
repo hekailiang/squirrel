5f2e8b1efb43ca517e64282f6f0a2026962ab1a1
hekailiang <hekailiang@gmail.com>
2015-07-15 11:50:01 +0800

    add test case for clone state machine builder

--- squirrel-foundation/src/test/java/org/squirrelframework/foundation/fsm/StateMachineImporterTest.java
+++ squirrel-foundation/src/test/java/org/squirrelframework/foundation/fsm/StateMachineImporterTest.java
@@ -3,7 +3,6 @@ package org.squirrelframework.foundation.fsm;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.squirrelframework.foundation.exception.SquirrelRuntimeException;
 import org.squirrelframework.foundation.fsm.annotation.StateMachineParameters;
 import org.squirrelframework.foundation.fsm.annotation.Transit;
 import org.squirrelframework.foundation.fsm.annotation.Transitions;
@@ -19,15 +18,12 @@ public class StateMachineImporterTest {
         @Transit(from="a", to="b", on="toB")
     })
     @StateMachineParameters(stateType=String.class, eventType=TestEvent.class, contextType=Integer.class)
-    class ImportParameterizedActionCase extends AbstractUntypedStateMachine {
+    static class ImportParameterizedActionCase extends AbstractUntypedStateMachine {
     }
     
-    @Test(expected=SquirrelRuntimeException.class)
-    @SuppressWarnings("unused")
+    @Test(expected=IllegalArgumentException.class)
     public void testImportParameterizedActionFail() {
-        final Integer param1 = 10;
-        final String param2 = "Hello World!";
-        UntypedStateMachineBuilder builder = 
+        UntypedStateMachineBuilder builder =
                 StateMachineBuilderFactory.create(ImportParameterizedActionCase.class);
         builder.externalTransition().from("a").to("b").on(TestEvent.toB).perform(new UntypedAnonymousAction() {
             @Override
@@ -36,34 +32,42 @@ public class StateMachineImporterTest {
             }
         });
         ImportParameterizedActionCase sample = builder.newUntypedStateMachine("a");
-        
-        UntypedStateMachineBuilder importedBuilder = 
-                new UntypedStateMachineImporter().importDefinition(sample.exportXMLDefinition(false));
+
+        String definition = sample.exportXMLDefinition(false);
+        UntypedStateMachineBuilder importedBuilder =  new UntypedStateMachineImporter().importDefinition(definition);
     }
     
-    @Test(expected=SquirrelRuntimeException.class)
-    @SuppressWarnings("unused")
-    public void testImportParameterizedActionSuccess() {
-        final Integer param1 = 10;
-        final String param2 = "Hello World!";
-        UntypedStateMachineBuilder builder = 
+    @Test
+    public void testImportNonParameterizedActionSuccess() {
+        UntypedStateMachineBuilder builder =
                 StateMachineBuilderFactory.create(ImportParameterizedActionCase.class);
-        builder.externalTransition().from("a").to("b").on(TestEvent.toB).perform(new UntypedAnonymousAction() {
-            {
-                // user need to make sure there always no-args constructor exits
-            }
-            @Override
-            public void execute(Object from, Object to, Object event, Object context, 
-                    UntypedStateMachine stateMachine) {
-            }
-        });
+        builder.externalTransition().from("a").to("b").on(TestEvent.toB).perform(Action.DUMMY_ACTION);
         ImportParameterizedActionCase sample = builder.newUntypedStateMachine("a");
         
         UntypedStateMachineBuilder importedBuilder = 
                 new UntypedStateMachineImporter().importDefinition(sample.exportXMLDefinition(false));
         ImportParameterizedActionCase importedSample = importedBuilder.newAnyStateMachine("a");
         importedSample.fire(TestEvent.toB);
-        assertTrue(importedSample.getCurrentState().equals("a"));
+        assertTrue(importedSample.getCurrentState().equals("b"));
+    }
+
+    @Test
+    public void testCloneStateMachineBuilder() {
+        UntypedStateMachineBuilder builder =
+                StateMachineBuilderFactory.create(ImportParameterizedActionCase.class);
+        ImportParameterizedActionCase orgSample = builder.newUntypedStateMachine("a");
+        orgSample.fire(TestEvent.toB);
+
+        UntypedStateMachineBuilder clonedBuilder =
+                new UntypedStateMachineImporter().importDefinition(orgSample.exportXMLDefinition(false));
+        // add new transition after clone a new state machine builder
+        clonedBuilder.externalTransition().from("a").to("c").on(TestEvent.toC);
+
+        ImportParameterizedActionCase newSample = clonedBuilder.newAnyStateMachine("a");
+        newSample.fire(TestEvent.toC);
+
+        assertTrue(orgSample.getCurrentState().equals("b"));
+        assertTrue(newSample.getCurrentState().equals("c"));
     }
 
 }
